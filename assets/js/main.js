84bbb1068e938a682da7bce7610cab0d68d880cb
Akhi111 <acharekarakhil@gmail.com>
2024-07-20 22:30:35 +0530

    fix style & animation for certificate

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,4 +144,4 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
 sr.reveal(`.home__name, .home__info, about__container .section__title-1, .about__info, .contact__social, contact__data`, {origin: 'left'})
-sr.reveal(`.services__card, .projects__card`, {interval: 100})
+sr.reveal(`.services__card, .projects__card, .certificate__card`, {interval: 100})
